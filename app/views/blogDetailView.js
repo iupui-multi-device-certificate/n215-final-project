4f192284dcbfa8135ddd21c5c5a473ba897f2bae
SimpleNotSimpler <simplenotsimpler@outlook.com>
2023-03-04 09:23:54 -0500

    feature: blog banner basic

--- app/views/blogDetailView.js
+++ app/views/blogDetailView.js
@@ -1,11 +1,13 @@
 import { formatMonth, formatDay } from "../helpers.js";
 
 export const blogDetailView = (blog) => `
-    <article>      
-        <img src="${blog.relativeImagePath}" alt="" />
-        <div class="date-block-large">
-          <div class="day">${formatDay(blog.datePublished)}</div>
-          <div class="month upper">${formatMonth(blog.datePublished)}</div>
-        </div>  
+    <article class="blog-post-detail content-width">
+        <div class="blog-banner">    
+          <img src="${blog.relativeImagePath}" alt="" />
+          <div class="date-block-large">
+            <div class="day">${formatDay(blog.datePublished)}</div>
+            <div class="month upper">${formatMonth(blog.datePublished)}</div>
+          </div> 
+        </div>
     </article>
 `;
