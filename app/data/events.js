2ae8f542938b47e533ab419eff991c4224f37359
SimpleNotSimpler <simplenotsimpler@outlook.com>
2023-03-03 20:23:36 -0500

    feature: events data

---
+++ app/data/events.js
@@ -0,0 +1,62 @@
+export const blogs = [
+  {
+    id: 1,
+    relativeImagetPath: "./images/gallery/1.png",
+    datePublished: "2023-06-06",
+    title: "Sed et persipiatis unde omnis iste natus",
+    summary: `      <p>
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid temporibus quos aspernatur
+  </p>`,
+    details: `<p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. </p>
+    <p>Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. </p>
+  `,
+  },
+  {
+    id: 2,
+    relativeImagetPath: "./images/gallery/2.jpeg",
+    datePublished: "2023-07-30",
+    title: "Sed et persipiatis unde omnis iste natus",
+    summary: `      <p>
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid temporibus quos aspernatur
+  </p>`,
+    details: `<p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. </p>
+  <p>Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. </p>
+`,
+  },
+  {
+    id: 3,
+    relativeImagetPath: "./images/gallery/3.jpeg",
+    datePublished: "2023-08-30",
+    title: "Sed et persipiatis unde omnis iste natus",
+    summary: `      <p>
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid temporibus quos aspernatur
+  </p>`,
+    details: `<p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. </p>
+  <p>Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. </p>
+`,
+  },
+  {
+    id: 4,
+    relativeImagetPath: "./images/gallery/4.jpeg",
+    datePublished: "2023-11-23",
+    title: "Sed et persipiatis unde omnis iste natus",
+    summary: `      <p>
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid temporibus quos aspernatur
+  </p>`,
+    details: `<p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. </p>
+  <p>Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. </p>
+`,
+  },
+  {
+    id: 5,
+    relativeImagetPath: "./images/gallery/5.jpeg",
+    datePublished: "2023-12-23",
+    title: "Sed et persipiatis unde omnis iste natus",
+    summary: `      <p>
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid temporibus quos aspernatur
+  </p>`,
+    details: `<p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. </p>
+  <p>Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. </p>
+`,
+  },
+];
