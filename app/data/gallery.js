fcea010b8d2017c520d00cffeeb511010211a5d4
SimpleNotSimpler <simplenotsimpler@outlook.com>
2023-03-05 10:48:50 -0500

    feature: set up gallery images

--- app/data/gallery.js
+++ app/data/gallery.js
@@ -2,6 +2,7 @@ export const events = [
   {
     id: 1,
     relativeImagePath: "./images/gallery/1.png",
+    relativeImagePathSmall: "./images/gallery/1-small.png",
     title: "food festival",
     details: `<p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. </p>
     <p>Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. </p>
@@ -9,7 +10,8 @@ export const events = [
   },
   {
     id: 2,
-    relativeImagePath: "./images/gallery/2.jpeg",
+    relativeImagePath: "./images/gallery/2.png",
+    relativeImagePathSmall: "./images/gallery/2-small.png",
     title: "dee-jay",
     details: `<p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. </p>
   <p>Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. </p>
@@ -17,7 +19,8 @@ export const events = [
   },
   {
     id: 3,
-    relativeImagePath: "./images/gallery/3.jpeg",
+    relativeImagePath: "./images/gallery/3.png",
+    relativeImagePathSmall: "./images/gallery/3-small.png",
     title: "speech",
     details: `<p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. </p>
   <p>Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. </p>
@@ -25,7 +28,8 @@ export const events = [
   },
   {
     id: 4,
-    relativeImagePath: "./images/gallery/4.jpeg",
+    relativeImagePath: "./images/gallery/4.png",
+    relativeImagePathSmall: "./images/gallery/4-small.png",
     title: "open foodfest",
     details: `<p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. </p>
   <p>Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. </p>
@@ -33,7 +37,8 @@ export const events = [
   },
   {
     id: 5,
-    relativeImagePath: "./images/gallery/5.jpeg",
+    relativeImagePath: "./images/gallery/5.png",
+    relativeImagePathSmall: "./images/gallery/5-small.png",
     title: "international",
     details: `<p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. </p>
   <p>Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. </p>
